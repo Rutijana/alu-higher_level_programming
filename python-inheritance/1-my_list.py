e7af908074590bd62bda1934888e576e8b898504
Rutijana <j.rutijana@alustudent.com>
2025-03-07 12:17:09 -0800

    yuckk

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
-"""Defines a class MyList that inherits from list"""
+""" Class that inherits the attributes references of class list
+    Args:
+        list: class list
+"""
 
 
 class MyList(list):
-    """Class that inherits from list.
+    """ Class that inherits the attributes references of class list
 
     Args:
-        list (list): list to sort in ascending order.
+        list: class list
     """
+
     def print_sorted(self):
-        """Prints a list in ascending order.
-        """
-        list_ = self[:]
-        list_.sort()
-        print(list_)
+        """ Method that prints the sorted list """
+        l_sorted = self.copy()
+        l_sorted.sort()
+        print(l_sorted)
