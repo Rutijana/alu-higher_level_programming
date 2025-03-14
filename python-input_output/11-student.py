041bbeb290e083b90ff22235bdb1e7ac36b5f94c
Rutijana <j.rutijana@alustudent.com>
2025-03-14 04:24:53 -0700

    11-student.py

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -1,56 +1,38 @@
 #!/usr/bin/python3
-#Defining the class Student
-"""Module defining the class Student based on 10-student.py"""
+'''class that makes a student'''
 
 
 class Student:
-    """
-    Class that defines properties of student.
+    '''student class'''
+    first_name = None
+    last_name = None
+    age = None
 
-    Attributes:
-        first_name (str): first name of student.
-        last_name (int): last name of student.
-        age (int): age of student.
-    """
-    def __init__(self, first_name, last_name, age):
-        """Creates new instances of Student.
-
-        Args:
-            first_name (str): first name of student.
-            last_name (int): last name of student.
-            age (int): age of student.
-        """
+    def _init_(self, first_name, last_name, age):
+        '''inits the data needed'''
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """Retrieves a dictionary representation of a Student instance.
-
-        If attrs is a list of strings, only attribute names contained in,
-        this list must be retrieved.
-        Otherwise, all attributes must be retrieved.
-
-        Returns:
-            dict: dictionary representation.
-        """
-        if attrs is None:
-            return self.__dict__
-
-        new_dict = {}
-        for item in attrs:
-            try:
-                new_dict[item] = self.__dict__[item]
-            except Exception:
-                pass
-        return new_dict
+        '''returns a dict rep of the data'''
+        context = {
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'age': self.age
+        }
+        if attrs is None or type(attrs) != list:
+            return context
+        else:
+            cont = {}
+            for item in attrs:
+                if type(item) != str:
+                    return context
+                if item in context.keys():
+                    cont[item] = context[item]
+            return cont
 
     def reload_from_json(self, json):
-        """Replaces all attributes of the Student instance.
-
-        Args:
-            json (dict): json object.
-        """
-        # print("Type json --> {}".format(type(json)))
-        self.__dict__.update(json)
-
+        '''reload_from_json'''
+        for item in json.keys():
+            self._dict_[item] = json[item]
